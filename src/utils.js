47876e0a0d93c17a09eac6fb038c8f9309967e33
Mojib Mohammad <mmohammad3@outlook.com>
2022-12-01 22:15:56 -0800

    change omdbapi url to use https

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,12 @@
 export function getMoviesByName(name, type) {
   return fetch(
-    `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${name}&type=${type}`
+    `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${name}&type=${type}`
   );
 }
 
 export function getMovieById(movieId) {
   return fetch(
-    `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&i=${movieId}`
+    `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&i=${movieId}`
   );
 }
 
